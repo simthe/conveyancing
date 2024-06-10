ea69a6f2dfddb604dd41198b07402e3172525bf2
Simthe <simthenogqala@gmail.com>
2024-06-10 22:45:40 +0200

    home page change

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from "react";
 
 const HomePage: FunctionComponent = () => {
-    return <div>Home Page</div>
+    return <div>Home Page CHange</div>
 }
 
 export default HomePage;
